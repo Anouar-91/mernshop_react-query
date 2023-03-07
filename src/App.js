c7f649883d296d71db1dcf965d127946d860b786
Anouar-91 <a.berrouane@hotmail.com>
2023-03-07 11:39:30 +0100

    first query and install react query

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
 import Footer from "./components/Footer";
 import Header from "./components/Header";
-import 'bootstrap/dist/css/bootstrap.min.css';
+import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap";
 import HomeScreen from "./screens/HomeScreen";
-import {BrowserRouter, Route, Routes} from 'react-router-dom';
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import ProductScreen from "./screens/ProductScreen";
 import CartScreen from "./screens/CartScreen";
 import LoginScreen from "./screens/LoginScreen";
@@ -18,42 +18,57 @@ import UserEditScreen from "./screens/UserEditScreen";
 import ProductListScreen from "./screens/ProductListScreen";
 import ProductEditScreen from "./screens/ProductEditScreen";
 import OrderListScreen from "./screens/OrderListScreen";
+import { QueryClient, QueryClientProvider } from "react-query";
+
+const queryClient = new QueryClient();
 
 function App() {
   return (
-    <BrowserRouter>
-
-      <Header></Header>
-      <main>
-        <div className="container-fluid">
-        <Routes>
-        <Route path='/' element={<HomeScreen />} exact/>
-        <Route path='/product/:id' element={<ProductScreen />} />
-        <Route path='/cart/:id' element={<CartScreen />} />
-        <Route path='/cart' element={<CartScreen />} />
-        <Route path='/login' element={<LoginScreen />} />
-        <Route path='/register' element={<RegisterScreen />} />
-        <Route path='/profile' element={<ProfileScreen />} />
-        <Route path='/shipping' element={<ShippingScreen />} />
-        <Route path='/payment' element={<PaymentScreen />} />
-        <Route path='/placeorder' element={<PlaceOrderScreen />} />
-        <Route path='/order/:id' element={<OrderScreen />} />
-        <Route path='/admin/userlist' element={<UserListScreen />} />
-        <Route path='/admin/user/:id/edit' element={<UserEditScreen />} />
-        <Route path='/admin/product/:id/edit' element={<ProductEditScreen />} />
-        <Route path='/admin/productlist' element={<ProductListScreen />} exact />
-        <Route path='/admin/productlist/:pageNumber' element={<ProductListScreen />} />
-        <Route path='/admin/orderlist' element={<OrderListScreen />} />
-        <Route path='/search/:keyword' element={<HomeScreen />}  />
-        <Route path='/page/:pageNumber' element={<HomeScreen />}  />
-        <Route path='search/:keyword/page/:pageNumber' element={<HomeScreen />}  />
-
-
-        </Routes>
-        </div>
-      </main>
-      <Footer />
-    </BrowserRouter>
+    <QueryClientProvider client={queryClient}>
+      <BrowserRouter>
+        <Header></Header>
+        <main>
+          <div className="container-fluid">
+            <Routes>
+              <Route path="/" element={<HomeScreen />} exact />
+              <Route path="/product/:id" element={<ProductScreen />} />
+              <Route path="/cart/:id" element={<CartScreen />} />
+              <Route path="/cart" element={<CartScreen />} />
+              <Route path="/login" element={<LoginScreen />} />
+              <Route path="/register" element={<RegisterScreen />} />
+              <Route path="/profile" element={<ProfileScreen />} />
+              <Route path="/shipping" element={<ShippingScreen />} />
+              <Route path="/payment" element={<PaymentScreen />} />
+              <Route path="/placeorder" element={<PlaceOrderScreen />} />
+              <Route path="/order/:id" element={<OrderScreen />} />
+              <Route path="/admin/userlist" element={<UserListScreen />} />
+              <Route path="/admin/user/:id/edit" element={<UserEditScreen />} />
+              <Route
+                path="/admin/product/:id/edit"
+                element={<ProductEditScreen />}
+              />
+              <Route
+                path="/admin/productlist"
+                element={<ProductListScreen />}
+                exact
+              />
+              <Route
+                path="/admin/productlist/:pageNumber"
+                element={<ProductListScreen />}
+              />
+              <Route path="/admin/orderlist" element={<OrderListScreen />} />
+              <Route path="/search/:keyword" element={<HomeScreen />} />
+              <Route path="/page/:pageNumber" element={<HomeScreen />} />
+              <Route
+                path="search/:keyword/page/:pageNumber"
+                element={<HomeScreen />}
+              />
+            </Routes>
+          </div>
+        </main>
+        <Footer />
+      </BrowserRouter>
+    </QueryClientProvider>
   );
 }
 
--- src/screens/HomeScreen.jsx
+++ src/screens/HomeScreen.jsx
@@ -1,60 +1,78 @@
-import React, { useEffect, useState } from 'react'
-import Product from '../components/Product';
-import {ThreeDots} from  'react-loader-spinner'
+import React, { useEffect, useState } from "react";
+import Product from "../components/Product";
+import { ThreeDots } from "react-loader-spinner";
 //redux
-import { useDispatch, useSelector } from 'react-redux';
-import Message from '../components/Message';
-import { listProducts } from '../redux-toolkit/reducers/productReducer';
-import { useParams } from 'react-router-dom';
-import Paginate from '../components/Paginate';
-import ProductCarousel from '../components/ProductCarousel';
-import Meta from '../components/Meta';
-
+import { useDispatch, useSelector } from "react-redux";
+import Message from "../components/Message";
+import { listProducts } from "../redux-toolkit/reducers/productReducer";
+import { useParams } from "react-router-dom";
+import Paginate from "../components/Paginate";
+import ProductCarousel from "../components/ProductCarousel";
+import Meta from "../components/Meta";
+import { useQuery } from "react-query";
+import axios from "axios";
 
 const HomeScreen = () => {
-    let {keyword} = useParams();
-    let { pageNumber } = useParams();
-    const dispatch = useDispatch();
-    const productsList = useSelector(state => state.productsList)
-    const { loading, error, products, page, pages } = productsList
+  let { keyword } = useParams();
+  let { pageNumber } = useParams();
+  //const dispatch = useDispatch();
+  //const productsList = useSelector(state => state.productsList)
+  //const { loading, error, products, page, pages } = productsList
 
-    useEffect(() => {
+  /*     useEffect(() => {
         dispatch(listProducts({keyword, pageNumber}))
-    }, [dispatch, keyword, pageNumber])
+    }, [dispatch, keyword, pageNumber]) */
 
+  const fetchProducts = async () => {
+    const { data } = await axios.get(
+      process.env.REACT_APP_API_URL +
+        "products?keyword=" +
+        keyword +
+        "&pageNumber=" +
+        pageNumber
+    );
+    return data;
+  };
+  //react query
+  const { isLoading, error, data } = useQuery("productAll", () =>
+    fetch(process.env.REACT_APP_API_URL + 'products').then((res) => res.json())
+  );
+  console.log(data);
+  //end
 
+  return (
+    <div className="container">
+      <Meta />
 
-    return (
-        <div className="container">
-                        <Meta />
+      {/*                {!keyword && <><div className="mt-3"><ProductCarousel /></div></>} */}
 
-                        {!keyword && <><div className="mt-3"><ProductCarousel /></div></>}
-
-            <h1>Latest Products</h1>
-            {loading 
-            ? <ThreeDots wrapperStyle={{justifyContent: 'center'}} /> 
-            : error 
-            ? (<Message variant="danger">{error}</Message>) :
-            <>
-            <div className="row">
-                {products.map(product => {
-                    return (
-                        <div key={product._id} className="col-md-4">
-                            <Product product={product} />
-                        </div>
-                    )
-
-                })}
-            </div>
-            <div className="mt-5">
-                <Paginate pages={pages} page={page} keyword={keyword ? keyword : ""} />
-            </div>
+      <h1>Latest Products</h1>
+      {isLoading ? (
+        <ThreeDots wrapperStyle={{ justifyContent: "center" }} />
+      ) : error ? (
+        <Message variant="danger">{error}</Message>
+      ) : (
+        <>
+          <div className="row">
+            {data.products.map((product) => {
+              return (
+                <div key={product._id} className="col-md-4">
+                  <Product product={product} />
+                </div>
+              );
+            })}
+          </div>
+{/*           <div className="mt-5">
+            <Paginate
+              pages={data.pages}
+              page={data.page}
+              keyword={keyword ? keyword : ""}
+            />
+          </div> */}
         </>
+      )}
+    </div>
+  );
+};
 
-            }
-
-        </div>
-    )
-}
-
-export default HomeScreen
+export default HomeScreen;
